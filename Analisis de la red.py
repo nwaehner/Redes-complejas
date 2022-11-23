70dce308e42d0cf128231961b9fcb1dbde3a3843
Manuel Delgado <manueldelgadouba@gmail.com>
2022-11-22 22:02:01 -0300

    Agrego analisis de asortatividad por popularidad

--- Analisis de la red.py
+++ Analisis de la red.py
@@ -43,6 +43,12 @@ print('Kmin = '+str(Kmin))
 print('gamma = '+str(gamma))
 p,sims = ajuste_grafo.test_pl(usefortran=True, niter=10000, nosmall=False)
 print(f"El p_valor de la red es {p}")
+
+
+#%% KRETUREW NO ANDAAAA
+
+G.nodes()["KR3TURE"]["popularidad"] = np.nan
+
 #%%--------------------Análisis de la asortatividad por Barabási-----------------------
 
 def f_ajuste(x, a, mu): return a*x**mu
@@ -50,10 +56,11 @@ def f_ajuste(x, a, mu): return a*x**mu
 # Creamos un dataframe con el grado y el grado medio de los vecinosdataframe 
 df = pd.DataFrame(dict(
     GRADO = dict(G.degree),
-    GRADO_MEDIO_VECINOS = nx.average_neighbor_degree(G)
-)) 
+    GRADO_MEDIO_VECINOS = nx.average_neighbor_degree(G),
+    POPULARIDAD = {nodo: G.nodes()[nodo]["popularidad"] for nodo in G.nodes()}
+    )) 
 
-#calculo el grado medio por grado para ambas redes
+#calculo el grado medio por grado
 grado_medio_por_grado = df.groupby(['GRADO'])['GRADO_MEDIO_VECINOS'].mean()
 
 # Defino las variables x e y de lo que voy a ajustar
@@ -77,17 +84,111 @@ var_y_ajuste = f_ajuste(var_x_ajuste, a, mu)
 
 
 # Graficamos
-fig, axs = plt.subplots(figsize = (16,6), facecolor='#f4e8f4')
+fig, axs = plt.subplots(figsize = (8,6), facecolor='#f4e8f4')
 axs.loglog(grado_medio_por_grado.index,grado_medio_por_grado.values,'.',color='#901c8e', alpha= 0.8, label = "Datos")
 axs.loglog(var_x_ajuste,var_y_ajuste,color='g', alpha= 0.8, label = "Ajuste")
 axs.grid('on', linestyle = 'dashed', alpha = 0.5)
-axs.set_title("Red ",fontsize = 16)
 axs.set_xlabel("Grado",fontsize = 16)
 axs.set_ylabel("Grado medio de los vecinos",fontsize = 16)
-axs.legend(True)
+axs.legend(fontsize = 16)
+axs.tick_params(axis='both', which='major', labelsize=14)
 plt.show()
-#%%--------------------Análisis de la asortatividad por Newman-----------------------
+#%%--------------------Análisis de la asortatividad de popularidad-----------------------
+# Iteramos sobre los nodos y los vecinos para calcular la popularidad media de los vecinos
 
+lista_nodos = G.nodes()
+popularidad_media_vecinos = []
+
+for nodo in lista_nodos:
+    popularidad_media = 0
+    lista_vecinos = list(G.neighbors(nodo))
+    for vecino in lista_vecinos:
+        popularidad_media += df["POPULARIDAD"][nodo]
+
+    popularidad_media = popularidad_media/len(lista_vecinos)
+    popularidad_media_vecinos.append(popularidad_media)
+# Agregamos la columna al dataframe
+df["POPULARIDAD_MEDIA_VECINOS"] = popularidad_media_vecinos
+
+# Calculamos la popularidad media de los vecinos teniendo en cuenta la cant de colaboraciones
+popularidad_media_vecinos_por_cancion = []
+for nodo in lista_nodos:
+    popularidad_media = 0
+    lista_vecinos = list(G.neighbors(nodo))
+    for vecino in lista_vecinos:
+        cant_colaboraciones = len(G[nodo][vecino])
+        popularidad_media += (df["POPULARIDAD"][nodo]*cant_colaboraciones)
+
+    popularidad_media = popularidad_media/len(lista_vecinos)
+    popularidad_media_vecinos_por_cancion.append(popularidad_media)
+# Agregamos la columna al dataframe
+df["POPULARIDAD_MEDIA_VECINOS_POR_CANCION"] = popularidad_media_vecinos_por_cancion
+
+#%%------------------Pasamos a graficar la popularidad media de los vecinos-----------------------------------
+
+#NO ME ACUERDO APRA QUE ESTABA LS SIGUIENTE LINEAAA, LA COPIE DE LO QUE HICIMOS PARA EL GRADO
+
+#calculo la popularidad media por popularidad
+popularidad_media_por_popularidad = df.groupby(['POPULARIDAD'])['POPULARIDAD_MEDIA_VECINOS'].mean()
+
+# Defino las variables x e y de lo que voy a ajustar
+var_x1 = popularidad_media_por_popularidad.index 
+var_y1 = popularidad_media_por_popularidad.values
+
+# Utilizo curve_fit() para el ajuste
+popt, pcov = curve_fit(f_ajuste, var_x1, var_y1)
+
+# Imprimo en pantalla los valores de popt y pcov
+a1, mu1 = popt
+err_a1, err_mu1 = np.sqrt(np.diag(pcov))
+print("Los parametros de ajuste son:")
+print(f'a1: {a1} ± {err_a1}')
+print(f'mu1: {mu1} ± {err_mu1}')
+
+
+var_x1_ajuste = np.linspace(1, max(var_x1),100)
+var_y1_ajuste = f_ajuste(var_x1_ajuste, a1, mu1)
+
+#calculo la popularidad media por cancion por popularidad
+popularidad_media_por_cancion_por_popularidad = df.groupby(['POPULARIDAD'])['POPULARIDAD_MEDIA_VECINOS_POR_CANCION'].mean()
+
+# Defino las variables x e y de lo que voy a ajustar
+var_x2 = popularidad_media_por_cancion_por_popularidad.index 
+var_y2 = popularidad_media_por_cancion_por_popularidad.values
+
+# Utilizo curve_fit() para el ajuste
+popt, pcov = curve_fit(f_ajuste, var_x2, var_y2)
+
+# Imprimo en pantalla los valores de popt y pcov
+a2, mu2 = popt
+err_a2, err_mu2 = np.sqrt(np.diag(pcov))
+print("Los parametros de ajuste son:")
+print(f'a2: {a2} ± {err_a2}')
+print(f'mu2: {mu2} ± {err_mu2}')
+
+var_x2_ajuste = np.linspace(1, max(var_x2),100)
+var_y2_ajuste = f_ajuste(var_x2_ajuste, a, mu2)
+
+#%%------------------------Graficamos las asortatividades----------------------
+fig, axs = plt.subplots(ncols = 2,figsize = (14,8), facecolor='#f4e8f4')
+axs[0].loglog(popularidad_media_por_popularidad.index,popularidad_media_por_popularidad.values,'.',color='#901c8e', alpha= 0.8, label = "Datos")
+axs[0].loglog(var_x1_ajuste,var_y1_ajuste,color='g', alpha= 0.8, label = "Ajuste")
+axs[0].grid('on', linestyle = 'dashed', alpha = 0.5)
+axs[0].set_xlabel("Popularidad",fontsize = 16)
+axs[0].set_ylabel("Popularidad media de los vecinos",fontsize = 16)
+axs[0].legend(fontsize = 16)
+axs[0].tick_params(axis='both', which='major', labelsize=16)
+axs[0].set_title("Sin considerar colaboraciones",fontsize = 18)
+
+axs[1].loglog(popularidad_media_por_cancion_por_popularidad.index,popularidad_media_por_cancion_por_popularidad.values,'.',color='#901c8e', alpha= 0.8, label = "Datos")
+axs[1].loglog(var_x2_ajuste,var_y2_ajuste,color='g', alpha= 0.8, label = "Ajuste")
+axs[1].grid('on', linestyle = 'dashed', alpha = 0.5)
+axs[1].set_xlabel("Popularidad",fontsize = 16)
+axs[1].set_ylabel("Popularidad media de los vecinos",fontsize = 16)
+axs[1].legend(fontsize = 16)
+axs[1].tick_params(axis='both', which='major', labelsize=16)
+axs[1].set_title("Considerando colaboraciones",fontsize = 18)
+plt.show()
 
 #%%
 #RESHUFFLEAR LOS ENLACES RANDOM Y RECOLOREAR. EN AMBOS CASOS CALCULAR HOMOFILIA.
@@ -95,15 +196,15 @@ plt.show()
 
 
 def calcular_homofilia(red):
-  homofilia_numerador = 0
-  for enlace in red.edges(data=True):
-      genero_artista1 = red.nodes()[enlace[0]]["genero"]
-      genero_artista2 = red.nodes()[enlace[1]]["genero"]
+    homofilia_numerador = 0
+    for enlace in red.edges(data=True):
+        genero_artista1 = red.nodes()[enlace[0]]["genero"]
+        genero_artista2 = red.nodes()[enlace[1]]["genero"]
       
-      if genero_artista1 == genero_artista2:
-          homofilia_numerador += 1
+        if genero_artista1 == genero_artista2:
+            homofilia_numerador += 1
 
-  return homofilia_numerador/len(red.edges())
+    return homofilia_numerador/len(red.edges())
 
 homofilia_real = calcular_homofilia(G)
 
