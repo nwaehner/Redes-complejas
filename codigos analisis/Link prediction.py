9bfe45e7a150321949269075f540fdb777f8288c
Nicolas Waehner <nicolaswaehner@gmail.com>
2022-11-27 21:00:16 -0300

    LINK PREDICTION ACTUALIZO SIMILARIDAD COSENO

--- codigos analisis/Link prediction.py
+++ codigos analisis/Link prediction.py
@@ -33,23 +33,18 @@ from sklearn.model_selection import GridSearchCV
 #       ¿Como encontrar la cant de dimensiones optimas?
 
 #%%-----------------------Cargamos el multigrafo---------------------------
-with open(f"red_filtrada/red_filtrada.gpickle", "rb") as f:
+with open(f"../red_filtrada/red_filtrada.gpickle", "rb") as f:
     G = pickle.load(f)
-
-    
-#%% NODE TO VEC:
-
+#%% ----------SIMILARIDAD COSENO PERO CON NODE2VEC----------------
 g_emb = n2v(G, dimensions=8)
 WINDOW = 1 # Node2Vec fit window
 MIN_COUNT = 1 # Node2Vec min. count
 BATCH_WORDS = 4 # Node2Vec batch words
-
 mdl = g_emb.fit(
     window=WINDOW,
     min_count=MIN_COUNT,
     batch_words=BATCH_WORDS
 )
-
 # create embeddings dataframe
 emb_df = (
     pd.DataFrame(
@@ -57,10 +52,6 @@ emb_df = (
         index = G.nodes
     )
 )
-
-
-
-# %%
 def predict_links_no_conect(G, df, article_id):
     
     # separate target article with all others
@@ -108,15 +99,59 @@ def predict_links_conect(G, df, article_id):
     similaridad_articles = [art[1] for art in similar_articles]
     return articles,similaridad_articles
 
+#%% SIMILARIDAD COSENO PERO A MANO.
+
+
+def cosine_similarity2(nodo1,nodo2):
+    vecinos_nodo1 = [i[1] for i in G.edges(nodo1)]
+    grado_nodo1 = G.degree(nodo1)
+    vecinos_nodo2 = [i[1] for i in G.edges(nodo2)]
+    grado_nodo2 = G.degree(nodo2)
+    interseccion = set(vecinos_nodo1).intersection(set(vecinos_nodo2))
+    denominador = np.sqrt(grado_nodo1*grado_nodo2)
+    return len(interseccion)/denominador
+        
+def predict_links_no_conect(G, artista):
+    
+    # other articles are all articles which the current doesn't have an edge connecting
+    all_nodes = G.nodes()
+    other_nodes = [n for n in all_nodes if ((n not in list(G.adj[artista])) and (n != artista))]
+    
+    # get similarity of current reader and all other readers
+    nodos_y_sim = []
+    for nodo in other_nodes:
+        nodos_y_sim.append([nodo,cosine_similarity2(artista,nodo)])
+    
+    nodos_y_sim.sort(key=lambda x: x[1],reverse=True)
+    nodos = [i[0] for i in nodos_y_sim]
+    sim = [i[1] for i in nodos_y_sim]
+    return nodos,sim
+
+def predict_links_conect(G, artista):
+    
+    # other articles are all articles which the current doesn't have an edge connecting
+    all_nodes = G.nodes()
+    other_nodes = [n for n in all_nodes if ((n in list(G.adj[artista])) and (n != artista))]
+    
+    # get similarity of current reader and all other readers
+    nodos_y_sim = []
+    for nodo in other_nodes:
+        nodos_y_sim.append([nodo,cosine_similarity2(artista,nodo)])
+    
+    nodos_y_sim.sort(key=lambda x: x[1],reverse=True)
+    nodos = [i[0] for i in nodos_y_sim]
+    sim = [i[1] for i in nodos_y_sim]
+    return nodos,sim
+
 
 #%%
 nodos_recorridos = []
 artistas_y_similaridades_conectados = []
 artistas_y_similaridades_no_conectados = []
 for nodo in list(G.nodes()):
-    nodos_sim_no_conect , sim_no_conect = predict_links_no_conect(G, emb_df, nodo)
+    nodos_sim_no_conect , sim_no_conect = predict_links_no_conect(G, nodo)
     
-    nodos_sim_conect , sim_conect = predict_links_conect(G, emb_df, nodo)
+    nodos_sim_conect , sim_conect = predict_links_conect(G,  nodo)
     
     nodos_recorridos.append(nodo)
     #Hago esto para no agregar similaridad de nodos dos veces.
@@ -141,6 +176,9 @@ ax.hist(bins[:-1], bins,weights=counts/max(counts),range = [0,1], rwidth = 0.80,
 counts, bins = np.histogram(similaridades_conectados, bins=30)
 ax.hist(bins[:-1], bins,weights=counts/max(counts),range = [0,1], rwidth = 0.80, facecolor='g', alpha=0.75,label= "Artistas enlazados")
 
+ax.vlines(x = np.mean(similaridades_conectados), ymin = 0, ymax = 1, linewidth = 3, linestyle = '-', alpha = 0.8, color = 'b', label = 'Media conectados')
+ax.vlines(x = np.mean(similaridades_no_conectados), ymin = 0, ymax = 1, linewidth = 3, linestyle = '-', alpha = 0.8, color = 'b', label = 'Media no conectados')
+
 ax.grid('on', linestyle = 'dashed', alpha = 0.5)
 ax.set_xlabel("Similaridad", fontsize=12)
 ax.set_ylabel("Frecuencia normalizada", fontsize=12)
@@ -149,95 +187,14 @@ ax.legend(loc = 'best')
 plt.savefig("Similaridad entre artistas.png")
 plt.show()
  
-# %%
+#%%
 def tomartercero(elem):
     return elem[2]
 artistas_y_similaridades_no_conectados.sort(key=tomartercero,reverse=True)
-print(artistas_y_similaridades_no_conectados[0:100])
-
-#%% NODE TO VEC:
-
-g_emb = n2v(G, dimensions=16)
-WINDOW = 1 # Node2Vec fit window
-MIN_COUNT = 1 # Node2Vec min. count
-BATCH_WORDS = 4 # Node2Vec batch words
-
-mdl = g_emb.fit(
-    window=WINDOW,
-    min_count=MIN_COUNT,
-    batch_words=BATCH_WORDS
-)
-
-# create embeddings dataframe
-emb_df = (
-    pd.DataFrame(
-        [mdl.wv.get_vector(str(n)) for n in G.nodes()],
-        index = G.nodes
-    )
-)
+print(artistas_y_similaridades_no_conectados[0:10])
+for i in artistas_y_similaridades_no_conectados:
+    if "Anibal Troilo" in i:
+        print(i)
+    
+    
 # %%
-# %% Predictor de géneros musicales, me agarro lo otro 
-#del otro codigo mientras para definirle un genero a cada uno
-
-generos_representativos = ["Trap","Jazz", "Pop", "Hip Hop", "Clas", 
-                           "r&b", "Tango", "Cumbia", "Indie", 
-                           "Chamame", "Tronica", "Fol", "Rock", "Punk", "Rap", "Metal",
-                           "reggae"]
-G_copia = G.copy()
-lista_nodos = list(G.nodes())
-Y = []
-nodos_sin_etiquetas = []  #artistas que no tienen genero musical
-for nodo in lista_nodos:
-    generos_nuevos =[]
-    generos_spotify = (G_copia.nodes())[nodo]["generos_musicales"]
-    # Recorremos los generos que creemos que son importantes
-    for genero in generos_representativos:
-        # Recorremos los generos de spotify del cantante
-        for genero_spotify in generos_spotify:
-            # Comprobamos que el genero entre en alguno de los generos de spotify
-            if genero.lower().replace(" ", "") in genero_spotify.lower().replace(" ", ""):
-                generos_nuevos.append(genero)
-    generos_nuevos = list(np.unique(generos_nuevos))
-    try:
-        G_copia.nodes[nodo]["label"] = generos_nuevos[0]
-        Y.append(generos_nuevos[0])
-    except:
-        nodos_sin_etiquetas.append(nodo)
-        G_copia.nodes[nodo]["label"] = np.nan
-        Y.append(np.nan)
-        
-emb_df['target'] = Y
-# clasificador: ensemble de árboles (random forest)
-from sklearn.ensemble import RandomForestClassifier
-
-emb_df.dropna(inplace=True)
-# %%
-#librerias utilizadas
-
-# selección de modelos
-from sklearn.model_selection import train_test_split
-# clasificador: red neuronal (perceptron multicapa)
-from sklearn.neural_network import MLPClassifier
-# clasificador: support vector classifier
-from sklearn.svm import SVC
-# clasificador: ensemble de árboles (random forest)
-from sklearn.ensemble import RandomForestClassifier
-
-#modelos de regresión
-from sklearn.neural_network import MLPRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
-
-#GridSearchCV  paraajuste de hiperparámetros con cross-validation
-from sklearn.model_selection import GridSearchCV  
-
-#One-hot encoding para las variables categóricas
-from sklearn.preprocessing import OneHotEncoder
-
-# performance (matriz de confusión)
-from sklearn.metrics import confusion_matrix
-#%%
-rfc = RandomForestClassifier(random_state=1)
-# %%
-y_level = data['level'].to_numpy() #target para clasificación por niveles
-X_train, X_test, y_train, y_test = train_test_split(X, y_level, test_size=0.3, random_state=15) #train-test split (70-30%)
