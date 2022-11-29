811bce635af728189e18f5ebae2d25efb06e9995
ariel-rojas <arirojas50@gmail.com>
2022-11-29 10:23:59 -0300

    Update comunidades.py

--- codigos analisis/comunidades.py
+++ codigos analisis/comunidades.py
@@ -18,6 +18,7 @@ G_ig = ig.Graph.from_networkx(G)
 # %%
 infomap = G_ig.community_infomap() #Infomap   
 louvain = com.best_partition(G) #Louvain
+com_bt = G_ig.community_edge_betweenness(clusters = 17, directed = False, weights = None) #betweenness
 # %%
 def cluster_to_dict(cluster, g):
     dic = {}
@@ -25,27 +26,37 @@ def cluster_to_dict(cluster, g):
         for n in c:
             dic[g.vs[n]['_nx_name']] = i
     return dic
-
+dic_betweenness = cluster_to_dict(com_bt.as_clustering(), G_ig)
 dic_louvain = com.best_partition(G)
 dic_infomap = cluster_to_dict(infomap, G_ig)
+
+print(com_bt) # Cantidad de elementos y de mergeos hechos
+
+#%% Grafico del dendrograma
+
 # %%
 #recorro los nodos por cada cluster y veo la cantidad de artistas
 #por género y me construyo una matriz de confusión
 
-generos_representativos = ["Trap","Jazz", "Pop", "HipHop", "Clasico", "Indie",
-                           "R&B", "Tango", "Cumbia",
-                           "Chamame", "Electronica", "Folklore", "Rock", "Punk", "Rap", "Metal",
-                           "Reggae","Alternative"]
+generos_representativos = ["Trap","HipHop","Rap","Rock","Pop","Alternative","R&B","Folklore","Tango", "Cumbia",
+                           "Jazz", "Clasico",
+                           "Chamame", "Electronica",  "Punk",  "Metal",
+                           "Reggae"]
+orden = [0,5,17,9,12,13,7,8,26,15,3,1,21,25,2,10,4,6,14,11,16,18,19,20,22,23,24]
 #pruebo con louvain
-clusters = max(dic_louvain.values())  #cantidad de clusteres
+clusters = max(dic_betweenness.values())  #cantidad de clusteres
 # %%
 matriz_confusion = np.zeros((clusters,len(generos_representativos)))
 for cluster in range(clusters):
-    nodos_en_cluster = [nodo for (nodo, value) in dic_louvain.items() if value == cluster] 
+    # indice = orden[cluster]
+    nodos_en_cluster = [nodo for (nodo, value) in dic_betweenness.items() if value == cluster] 
     for artista in nodos_en_cluster:
+        # G.nodes()[artista]['infomap'] = cluster
         if len(G.nodes()[artista]['generos_musicales'])>0:
             for genero in G.nodes()[artista]['generos_musicales']:
                 matriz_confusion[cluster,generos_representativos.index(genero)] +=1
+        # else:
+        #     matriz_confusion[cluster,len(generos_representativos)] +=1
 
 # %% dibujo la matriz de confusion
 fig, ax = plt.subplots(1,1, figsize=(8,8))
@@ -59,4 +70,6 @@ ax.set_xticklabels(generos_representativos, rotation = 90)
 ax.set_ylabel("Comunidades por cluster", fontsize=12)
 ax.set_xlabel("Géneros Musicales", fontsize=12)
 plt.show()
+# %%
+pickle.dump(G, open(f'red_filtrada_comunidades.gpickle', 'wb'))
 # %%
